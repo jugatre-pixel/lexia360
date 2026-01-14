c855a15a794729e9f64dbd249dc137abcbcf7d08
jugatre-pixel <jugatre@gmail.com>
2026-01-14 18:27:24 +0100

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,1 +1,11 @@
+from datetime import datetime
+from sqlmodel import SQLModel, Field
+
+class Usuario(SQLModel, table=True):
+    id_usuario: int | None = Field(default=None, primary_key=True)
+    nombre: str
+    email: str = Field(index=True)
+    hashed_password: str
+    rol: str = "cliente"
+    creado_en: datetime = Field(default_factory=datetime.utcnow)
 
