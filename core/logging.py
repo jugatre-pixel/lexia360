5a0b8bfd2f43a76f7e631040349cd295ac39dae2
jugatre-pixel <jugatre@gmail.com>
2026-01-14 18:26:59 +0100

    Update logging.py

--- core/logging.py
+++ core/logging.py
@@ -1,1 +1,10 @@
+import traceback
+from fastapi import Request
+
+async def catch_exceptions(request: Request, call_next):
+    try:
+        return await call_next(request)
+    except Exception:
+        traceback.print_exc()
+        raise
 
