8ee6e885e526be53ffac97541c79093915514a51
jugatre-pixel <jugatre@gmail.com>
2026-01-20 14:22:02 +0100

    Update db.py

--- core/db.py
+++ core/db.py
@@ -1,5 +1,18 @@
-from sqlmodel import create_engine
+from sqlmodel import SQLModel, create_engine, Session
 from app.core.config import settings
 
-engine = create_engine(settings.database_url, echo=False, pool_pre_ping=True)
 
+engine = create_engine(
+    settings.DATABASE_URL,
+    echo=False,
+    pool_pre_ping=True,
+)
+
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine, expire_on_commit=False) as session:
+        yield session
